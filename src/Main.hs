8e0a5c2a8b0b8b336cdd79b3f8f28d34af6f7432
ongyiren1994 <ongyiren@outlook.com>
2021-10-04 01:23:05 +0800

    Add option to parse file,rename haskell-template to mermaid-hs

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,23 @@
 
 module Main where
 
+import FlowChart
 import Main.Utf8 (withUtf8)
+import Options.Applicative as O
+import Text.Megaparsec
+
+opt :: O.Parser FilePath
+opt =
+  strOption
+    ( long "file with mermaid syntax"
+        <> metavar "FILE"
+        <> short 'f'
+    )
 
 main :: IO ()
 main = do
   withUtf8 $ do
-    putStrLn "Hello World"
-
+    let opts = info (opt <**> helper) (fullDesc <> progDesc "Parse mermaid syntax inside a file")
+    file <- execParser opts
+    content <- readFile file
+    parseTest (pDiagram <* eof) (fromString content)
